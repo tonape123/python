cbcfc90704583bb173fb103c40502d3fb9067890
tonape123 <tonapenamrata@gmail.com>
2022-08-04 14:39:56 +0530

    commit

---
+++ 3.py
@@ -0,0 +1,12 @@
+a=5
+b=10
+c=15
+if a>b:
+    if a>c:
+        print("a is big",a)
+    else:
+        print("c value is big",c)
+elif b>c:
+        print("b value is big",b)
+else:
+        print("c is big",c)    
